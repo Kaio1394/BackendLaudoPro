d6fc9e0493d79c3a6e8edf19dbfcd91c205253cc
Kaio <kaiocandidosantiago@gmail.com>
2025-10-07 00:25:48 -0300

    adicionado entity

--- src/main/java/com/kaiosantiago/laudopro/entity/Customer.java
+++ src/main/java/com/kaiosantiago/laudopro/entity/Customer.java
@@ -1,4 +1,31 @@
 package com.kaiosantiago.laudopro.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Id
+    @Column(columnDefinition = "BINARY(16)", updatable = false, nullable = false, unique = true)
+    private UUID uuid;
+
+    @Column(nullable = false)
+    private String fantasyName;
+
+    @Column(nullable = false, length = 14)
+    private String cnpj;
+
+    @Column(nullable = false, length = 18)
+    private String cnpjFormated;
+
+    @Column(nullable = false)
+    private String address;
+
+    public Customer() {
+        this.uuid = UUID.randomUUID();
+    }
 }
